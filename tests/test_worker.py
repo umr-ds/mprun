ff8d31ab7285844b551f1757e06b19025fbe4121
Markus Sommer <msommer@informatik.uni-marburg.de>
2026-04-09 12:26:12 +0200

    Worker main loop

--- tests/test_worker.py
+++ tests/test_worker.py
@@ -9,12 +9,12 @@ from hypothesis import strategies as st
 
 from mprun.models import Worker
 from mprun.server import DATA_PATH_ENV, app
-from mprun.worker import register
+from mprun.worker import checkin, register
 
 
 @given(name=st.text())
 def test_register(name: str) -> None:
-    """Test registration."""
+    """Test worker registration."""
     with TemporaryDirectory(delete=True) as data_dir:
         putenv(DATA_PATH_ENV, data_dir)
 
@@ -22,3 +22,14 @@ def test_register(name: str) -> None:
             worker = register(client=client, name=name)
             assert isinstance(worker, Worker)
             assert worker.name == name
+
+
+@given(name=st.text())
+def test_checkin(name: str) -> None:
+    """Test worker checkin."""
+    with TemporaryDirectory(delete=True) as data_dir:
+        putenv(DATA_PATH_ENV, data_dir)
+
+        with TestClient(app) as client:
+            worker = register(client=client, name=name)
+            checkin(client=client, self=worker)
