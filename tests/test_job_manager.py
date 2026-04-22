7a9fdab7e94d9988e04532879f150101c06323f5
Markus Sommer <msommer@informatik.uni-marburg.de>
2026-04-22 17:56:19 +0200

    Test job dispatching.

--- tests/test_job_manager.py
+++ tests/test_job_manager.py
@@ -6,12 +6,13 @@ from tempfile import TemporaryDirectory
 import pytest
 
 from mprun.job_manager import JobManager
+from mprun.models import JobState, Run
 from tests.helpers import EXAMPLE_JOB
 
 
 @pytest.mark.asyncio
 async def test_create() -> None:
-    """Test Job creation with randomised data."""
+    """Test Job creation."""
     with TemporaryDirectory(delete=True) as data_dir:
         data_path = Path(data_dir)
         manager = JobManager(data_path=data_path)
@@ -27,3 +28,34 @@ async def test_create() -> None:
         all_jobs = await manager.get_all()
         assert len(all_jobs) == 1
         assert all_jobs[0] == job
+
+
+@pytest.mark.asyncio
+async def test_dispatch() -> None:
+    """Test Job dispatching."""
+    with TemporaryDirectory(delete=True) as data_dir:
+        data_path = Path(data_dir)
+        manager = JobManager(data_path=data_path)
+
+        dispatched = await manager.dispatch_waiting_run(0)
+        assert dispatched is None
+
+        job = await manager.create_job(definition=EXAMPLE_JOB)
+
+        retrieved = await manager.get(jid=job.jid)
+        assert retrieved.state == JobState.WAITING
+
+        dispatched = await manager.dispatch_waiting_run(wid=0)
+        assert isinstance(dispatched, Run)
+        assert dispatched.jid == job.jid
+
+        retrieved = await manager.get(jid=job.jid)
+        assert retrieved.state == JobState.RUNNING
+
+        found = False
+        for run in retrieved.runs:
+            if run == dispatched:
+                found = True
+                assert run.state == JobState.RUNNING
+                break
+        assert found
